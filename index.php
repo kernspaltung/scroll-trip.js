e400f5fa64eb915939a4affba2e63029b2adbe8d
rodrigo-frenk <rodrigo.frenk@gmail.com>
2017-01-03 16:04:02 -0600

    using pointer events and mousewheel plugins

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
-   <?php
+<?php
 
 
-   // test content to use later:
-   ob_start();
+// test content to use later:
+ob_start();
 
-   ?>
+?>
 
-   <div class="test-container columns medium-6 large-3 panel">
-      <small>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, a.
-      </small>
-   </div>
-   <div class="test-container columns medium-6 large-3 panel">
-      <img src="http://fakeimg.pl/300x200/fea330/0ba1ca" alt="">
-   </div>
+<div class="test-container columns medium-6 large-3 panel">
+   <small>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, a.
+   </small>
+</div>
+<div class="test-container columns medium-6 large-3 panel">
+   <a href="#"><img src="http://fakeimg.pl/300x200/fea330/0ba1ca" alt=""></a>
+</div>
 
-   <?php
-   $sample_html = ob_get_contents();
-   ob_end_clean();
+<?php
+$sample_html = ob_get_contents();
+ob_end_clean();
 
-   ?>
+?>
 
 
 <!doctype html>
@@ -40,51 +40,55 @@
 <body>
 
    <div id="scroll-container" class="scroll-container">
-      <div class="empty-height-container">
 
+
+
+            <div class="empty-height-container">
+
+            </div>
+
+      <div class="travel-container">
+         <div class="travel">
+            <div id="level-1" class="level row expanded">
+               <div class="row expanded">
+                  <?php for ($i=0; $i < 2; $i++) {
+                     echo $sample_html;
+                  } ?>
+               </div>
+
+            </div>
+            <div id="level-2" class="level row expanded">
+               <div class="row expanded">
+                  <?php for ($i=0; $i < 2; $i++) {
+                     echo $sample_html;
+                  } ?>
+               </div>
+            </div>
+            <div id="level-3" class="level row expanded">
+               <div class="row expanded">
+                  Lorem ipsum dolor sit amet.
+                  <img src="http://fakeimg.pl/300x200/fea330/0ba1ca" alt="">
+
+               </div>
+            </div>
+            <div id="level-4" class="level row expanded">
+               <div class="row expanded">
+                  <?php for ($i=0; $i < 2; $i++) {
+                     echo $sample_html;
+                  } ?>
+               </div>
+            </div>
+            <div id="level-5" class="level row expanded">
+               <div class="row expanded">
+                  Lorem ipsum dolor sit amet.
+                  <img src="http://fakeimg.pl/300x200/fea330/0ba1ca" alt="">
+
+               </div>
+            </div>
+         </div>
       </div>
 
-   </div>
 
-   <div class="travel-container">
-      <div class="travel">
-         <div id="level-1" class="level row expanded">
-            <div class="row expanded">
-               <?php for ($i=0; $i < 2; $i++) {
-                  echo $sample_html;
-               } ?>
-            </div>
-
-         </div>
-         <div id="level-2" class="level row expanded">
-            <div class="row expanded">
-               <?php for ($i=0; $i < 2; $i++) {
-                  echo $sample_html;
-               } ?>
-            </div>
-         </div>
-         <div id="level-3" class="level row expanded">
-            <div class="row expanded">
-               Lorem ipsum dolor sit amet.
-               <img src="http://fakeimg.pl/300x200/fea330/0ba1ca" alt="">
-
-            </div>
-         </div>
-         <div id="level-4" class="level row expanded">
-            <div class="row expanded">
-               <?php for ($i=0; $i < 2; $i++) {
-                  echo $sample_html;
-               } ?>
-            </div>
-         </div>
-         <div id="level-5" class="level row expanded">
-            <div class="row expanded">
-               Lorem ipsum dolor sit amet.
-               <img src="http://fakeimg.pl/300x200/fea330/0ba1ca" alt="">
-
-            </div>
-         </div>
-      </div>
    </div>
 
 
@@ -95,6 +99,11 @@
    <script src="bower_components/jquery/dist/jquery.js"></script>
    <script src="bower_components/what-input/what-input.js"></script>
    <script src="bower_components/foundation-sites/dist/foundation.js"></script>
+   <script src="bower_components/jquery-mousewheel/jquery.mousewheel.min.js"></script>
+
+   <!-- Pointer Events Polyfill: -->
+   <script src="https://code.jquery.com/pep/0.4.1/pep.js"></script>
+
    <script src="js/app.js"></script>
 </body>
 </html>
